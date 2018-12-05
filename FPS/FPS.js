08b23a1f62f66924d42444e7331391bcd3618db6
chunyuexu <johnxu2000@outlook.com>
2018-12-05 16:19:17 -0500

    crosshair generator

--- FPS/FPS.js
+++ FPS/FPS.js
@@ -3,28 +3,30 @@ function draw ()
 {
 	var color = document.getElementById("selector_color").value;
 	
-	var length = document.getElementById("range_length_disp").value;
+	var length = parseInt(document.getElementById("range_length_disp").value);
 	
-	var gap = document.getElementById("range_gap_disp").value;
+	var gap = parseInt(document.getElementById("range_gap_disp").value);
 	
-	var thickness = document.getElementById("range_thickness_disp").value;
+	var thickness = parseInt(document.getElementById("range_thickness_disp").value);
 	
 	var canvas = document.getElementById("crosshairCanvas");
 	
     var ctx = canvas.getContext('2d');
 	
+	
 	//top
 	ctx.fillStyle = color;
 	
-	ctx.fillRect(100, 100 - gap, thickness, -length);
+	
+	ctx.fillRect(100-(thickness/2), 100-length-gap, thickness, length);
 	
 	//right
-	ctx.fillRect(100 + thickness + gap, 100, length, thickness);
+	ctx.fillRect(100+gap, 100-(thickness/2), length, thickness);
 	
 	//bottom
-	ctx.fillRect(100, 100 + thickness + gap, thickness, length);
+	ctx.fillRect(100-(thickness/2), 100+gap, thickness, length);
 	
 	//left
-	ctx.fillRect(100 - gap, 100, -length, thickness);
+	ctx.fillRect(100-length-gap, 100-(thickness/2), length, thickness);
 	
 }
